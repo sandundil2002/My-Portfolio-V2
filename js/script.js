b0d10d13d3f366ffc499b642fabd63c122433bd7
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2024-06-13 23:13:54 +0530

    home section implemented.

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ let third = document.getElementById("hr3");
 let menu = document.querySelector(".phoneMenu");
 let count = 0;
 
+/*------------------------------------Menu-Btn----------------------------------*/
 document.querySelector("menu").addEventListener("click", () => {
   if (count % 2 === 0) {
     showMenu();
@@ -57,4 +58,56 @@ window.onresize = () => {
       disableScroll();
     }
   }
+};
+
+/*------------------------------------Type-Writer----------------------------------*/
+
+const TypeWriter = function (txtElement, words, wait) {
+  this.txtElement = txtElement;
+  this.words = words;
+  this.txt = "";
+  this.wordIndex = 0;
+  this.wait = parseInt(wait, 10);
+  this.type();
+  this.isDeleting = false;
+};
+
+TypeWriter.prototype.type = function () {
+  const current = this.wordIndex % this.words.length;
+  const fulltxt = this.words[current];
+
+  if (this.isDeleting) {
+    this.txt = fulltxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fulltxt.substring(0, this.txt.length + 1);
+  }
+
+  this.txtElement.innerHTML = '<span class = "txt">' + this.txt + "</span>";
+
+  let typeSpeed = 100;
+  if (this.isDeleting) {
+    typeSpeed /= 2;
+  }
+
+  if (!this.isDeleting && this.txt === fulltxt) {
+    typeSpeed = this.wait;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === "") {
+    this.isDeleting = false;
+    this.wordIndex++;
+  }
+
+  setTimeout(() => this.type(), typeSpeed);
+};
+
+window.onload = function () {
+  const txtElement = document.querySelector(".role");
+  const words = JSON.parse(txtElement.getAttribute("data-words"));
+  const wait = txtElement.getAttribute("data-wait");
+  new TypeWriter(txtElement, words, wait);
+
+  const txtElementFooter = document.querySelector(".footer");
+  const wordsFooter = JSON.parse(txtElementFooter.getAttribute("data-words"));
+  const waitFooter = txtElementFooter.getAttribute("data-wait");
+  new TypeWriter(txtElementFooter, wordsFooter, waitFooter);
 };
