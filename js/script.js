4a3556233f0f40c946e29f3e3427a336eaaf667a
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2024-06-14 19:32:22 +0530

    nav-bar hide function implement.

--- js/script.js
+++ js/script.js
@@ -3,6 +3,10 @@ let second = document.getElementById("hr2");
 let third = document.getElementById("hr3");
 let menu = document.querySelector(".phoneMenu");
 let count = 0;
+let nav = document.querySelector("nav");
+let before = 0;
+let pos = 0;
+const navHeight = -nav.clientHeight; 
 
 /*------------------------------------Menu-Btn----------------------------------*/
 document.querySelector("menu").addEventListener("click", () => {
@@ -111,3 +115,31 @@ window.onload = function () {
   const waitFooter = txtElementFooter.getAttribute("data-wait");
   new TypeWriter(txtElementFooter, wordsFooter, waitFooter);
 };
+
+window.addEventListener("scroll", function (e) {
+  let after = this.scrollY; 
+  if (after != 0) {
+    nav.style.boxShadow = "0px 5px 15px black";
+  } else {
+    nav.style.boxShadow = "0px 0 0px black";
+  }
+
+  if (before < after) {
+    if (pos > navHeight) {
+      pos -= after - before;
+      if (pos < navHeight) {
+        pos = navHeight;
+      }
+      nav.style.top = pos + "px";
+    }
+  } else {
+    if (pos < 0) {
+      pos += before - after;
+      if (pos > 0) {
+        pos = 0;
+      }
+      nav.style.top = pos + "px";
+    }
+  }
+  before = after;
+});
